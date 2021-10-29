1117ed80dbea4fd97bccb9a9f0e580119825f448
vleonecezar <vleone.social@gmail.com>
2021-10-29 11:44:18 -0300

    favorites list state para mudar de pesquisa para busca de acordo com a opção escolhida

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
+import { useState } from "react";
 import { Layout } from "./app.styles";
 import FavoritesTittle from "./Components/FavoritesTittle";
 import SearchBar from "./Components/SearchBar";
 import { GlobalStyle } from "./Styles/GlobalStyles";
 
 function App() {
+  const [favoritesList, setFavoritesList] = useState(false);
+
   return (
     <>
       <GlobalStyle />
-      <Layout>
-        <FavoritesTittle />
-        {/* <SearchBar /> */}
-      </Layout>
+      <Layout>{favoritesList ? <FavoritesTittle /> : <SearchBar />}</Layout>
     </>
   );
 }
