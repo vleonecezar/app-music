22ccda7eed62db55c7a233281416d63e3f5760d4
vleonecezar <vleone.social@gmail.com>
2021-11-05 18:14:59 -0300

    redux persist

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,12 +1,21 @@
 import { createStore, combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { favoriteReducer } from "./Favorites/favorites.reducer";
 import { playerReducer } from "./Player/player.reducer";
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  whitelist: ["favoriteReducer"],
+};
 
 const rootReducer = combineReducers({
   favoriteReducer,
   playerReducer,
 });
 
-const store = createStore(rootReducer);
+const pReducer = persistReducer(persistConfig, rootReducer);
 
-export default store;
+export const store = createStore(pReducer);
+export const persistor = persistStore(store);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { persistor, store } from "./Store";
 import App from "./App";
-import store from "./Store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
